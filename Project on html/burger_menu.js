8830449cfe71b98d69ce2730bf95903b6900c18e
VadymChink <vadim-vsnnschuk@ro.ru>
2022-01-28 17:18:44 +0200

    end adaptive in  project on HTML

--- Project on html/burger_menu.js
+++ Project on html/burger_menu.js
@@ -1,7 +1,9 @@
 const btn = document.querySelector(  '.burger_menu') ;
 const menu = document.querySelector(  '.nav' ) ;
 const burger = document.querySelector(  '.burger_menu_item' );
+const scroll = document.querySelector(  'body' );
 btn.onclick = () => {
     menu.classList.toggle("active");
     burger.classList.toggle("active");
+    scroll.classList.toggle("lock");
 }
